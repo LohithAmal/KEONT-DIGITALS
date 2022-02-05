fb83ab5d75467f7595d2c41e743443ba571c3eeb
lohith <lohith05amal@gmail.com>
2022-02-05 16:19:43 -0500

    team section complete

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ $(window).on("load", function () {
   $("#status").fadeOut();
   $("#preloader").delay(350).fadeOut("slow");
 });
+
+// team section
+
+$(function () {
+  $("#team-members").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fas fa-angle-left"></i>',
+      '<i class="fas fa-angle-right"></i>',
+    ],
+  });
+});
